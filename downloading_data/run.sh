0f9fb83592d04872d2101c3d11e4df690c4bc127
mmajewsk <maciej.w.majewski@gmail.com>
2020-02-05 12:45:57 +0100

    modified run script

--- downloading_data/run.sh
+++ downloading_data/run.sh
@@ -1,3 +1,12 @@
+echo "Creating data_lartpc2d file"
+mkdir data_lartpc2d
+echo "Downloading data"
+cd data_lartpc2d
+wget -P data_lartpc2d http://www.stanford.edu/~kterao/public_data/v0.1.0/2d/segmentation/multipvtx/test_10k.root
+wget -P data_lartpc2d http://www.stanford.edu/~kterao/public_data/v0.1.0/2d/segmentation/multipvtx/train_15k.root
+echo "Building docker image"
 sudo docker build -t lartpc -f .Dockerfile .
+echo "Creating dump file"
 mkdir dump
+echo "Running docker and processing script"
 sudo docker run --rm -v $(pwd):/home/app/ -v $(pwd)/data_lartpc2d:/home/data --cpus=1 lartpc /bin/bash "cd /home/app; python3 process_lartpc_data.py --entries 10"
