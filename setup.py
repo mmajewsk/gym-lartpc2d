b17d2345c11321070333c9a238c86e92c8e51bf5
mmajewsk <maciej.w.majewski@gmail.com>
2020-09-02 12:17:14 +0200

    enabling automatic pypi publish

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+from version import VERSION
 
 with open("readme.md", "r") as fh:
     long_description = fh.read()
     setup(
         name='gym_lartpc',
-        version='0.0.2',
+        version=VERSION,
         author="Maciej Majewski",
         author_email="mmajewsk@cern.ch",
         description="Openai-gym compatible reinforcement learning env for lartpc data",
         long_description=long_description,
         long_description_content_type="text/markdown",
-        url="https://githum.com/mmajewsk/lartpc_game",
+        url="https://githum.com/mmajewsk/gym-lartpc2d",
+        packages=find_packages(),
         install_requires=[
                 "pandas==1.0.0",
                 "scipy==1.4.1",
