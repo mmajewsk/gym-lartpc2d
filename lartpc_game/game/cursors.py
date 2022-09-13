b6253f65d5662dced436cbe41ec439926171c419
mwmajewsk <5279578+mmajewsk@users.noreply.github.com>
2022-09-13 13:10:18 +0200

    blackify cursors and game

--- lartpc_game/game/cursors.py
+++ lartpc_game/game/cursors.py
@@ -1,8 +1,11 @@
 from lartpc_game.game.regions import Region2D, Region3D
 import numpy as np
 
+
 class Cursor:
-    def __init__(self, output_size, input_result_size, input_source_size, movement_size=3):
+    def __init__(
+        self, output_size, input_result_size, input_source_size, movement_size=3
+    ):
         """
 		:param output_size: output of the network size (the categorisation result, like one 1)
 		:param input_size:  input to the network size (usually 3x3x3)
@@ -15,12 +18,12 @@ class Cursor:
         self.region_output = self.__class__.region_cls(output_size)
         self.region_result_input = self.__class__.region_cls(input_result_size)
         self.region_source_input = self.__class__.region_cls(input_source_size)
-        self.region_movement =self.__class__.region_cls(movement_size)
+        self.region_movement = self.__class__.region_cls(movement_size)
         self.region_dict = {
-            'source_input':self.region_source_input,
-            'result_input': self.region_result_input,
-            'movement': self.region_movement,
-            'output': self.region_output
+            "source_input": self.region_source_input,
+            "result_input": self.region_result_input,
+            "movement": self.region_movement,
+            "output": self.region_output,
         }
 
     @property
@@ -41,32 +44,47 @@ class Cursor:
     def __update_indeces(self):
         self.__indeces = (self.current_center + self.region_movement.range).T
 
-    def get_range(self, arr: np.ndarray, center: np.ndarray = None, region_type='source_input') -> np.ndarray:
+    def get_range(
+        self, arr: np.ndarray, center: np.ndarray = None, region_type="source_input"
+    ) -> np.ndarray:
         if center is None:
             center = self.current_center
         region = self.region_dict[region_type]
         boundaries_low, boundaries_high = (region.r_low, region.r_high)
-        return self.__class__.region_cls.get_range(arr, center, boundaries_low, boundaries_high)
+        return self.__class__.region_cls.get_range(
+            arr, center, boundaries_low, boundaries_high
+        )
 
-    def set_range(self, arr: np.ndarray, value: np.ndarray, center: np.ndarray = None, region_type='output'):
+    def set_range(
+        self,
+        arr: np.ndarray,
+        value: np.ndarray,
+        center: np.ndarray = None,
+        region_type="output",
+    ):
         if center is None:
             center = self.current_center
         region = self.region_dict[region_type]
         boundaries_low, boundaries_high = (region.r_low, region.r_high)
-        return self.__class__.region_cls.set_range(arr, value, center, boundaries_low, boundaries_high)
+        return self.__class__.region_cls.set_range(
+            arr, value, center, boundaries_low, boundaries_high
+        )
 
     def copy(self):
         return type(self)(
             output_size=self._output_size,
-            input_result_size = self.input_result_size,
-            input_source_size = self.input_source_size,
-            movement_size=self._movement_size
+            input_result_size=self.input_result_size,
+            input_source_size=self.input_source_size,
+            movement_size=self._movement_size,
         )
+
 
 def _cursor_factory_by_region(RegionClass):
     class TmpCursor(Cursor):
         region_cls = RegionClass
+
     return TmpCursor
+
 
 Cursor2D = _cursor_factory_by_region(Region2D)
 Cursor3D = _cursor_factory_by_region(Region3D)
--- lartpc_game/game/game_ai.py
+++ lartpc_game/game/game_ai.py
@@ -1,13 +1,19 @@
 from lartpc_game.agents.settings import Action2DSettings, Observation2DSettings
-from lartpc_game.agents.observables import Action2Dai, State2Dai, Observation2Dai, State2Dai
+from lartpc_game.agents.observables import (
+    Action2Dai,
+    State2Dai,
+    Observation2Dai,
+    State2Dai,
+)
 from lartpc_game.game.cursors import Cursor2D
 import numpy as np
 import pandas as pd
 
 from abc import abstractmethod, ABC
 
+
 class DetectorMaps(ABC):
-    def __init__(self, result_dimensions = None):
+    def __init__(self, result_dimensions=None):
         self.source_map = None
         self.target_map = None
         self.result_map = None
@@ -16,18 +22,18 @@ class DetectorMaps(ABC):
     @property
     @abstractmethod
     def dimension_list(self):
-        #eg. return ['x','y','z']
+        # eg. return ['x','y','z']
         pass
 
     def read_source_nonzero_indeces(self):
-        self.nonzero_indeces = np.where(self.source_map > 0.)
+        self.nonzero_indeces = np.where(self.source_map > 0.0)
 
     def create_nonzero_df(self, nonzero_indeces):
         df_dict = {}
         for dim, nonzero_ax in zip(self.dimension_list, nonzero_indeces):
             df_dict[dim] = nonzero_ax
         self.nonzero_df = pd.DataFrame(df_dict)
-        self.nonzero_df['touched'] = 0
+        self.nonzero_df["touched"] = 0
 
     def get_random_positions(self):
         row = self.nonzero_df.sample(1)
@@ -40,7 +46,9 @@ class DetectorMaps(ABC):
             self.result_map = result
         else:
             if self.result_dimensions is not None:
-                self.result_map = np.zeros(self.target_map.shape+(self.result_dimensions,))
+                self.result_map = np.zeros(
+                    self.target_map.shape + (self.result_dimensions,)
+                )
             else:
                 self.result_map = np.zeros_like(self.target_map)
 
@@ -52,26 +60,29 @@ class DetectorMaps(ABC):
 
 
 class Detector2D(DetectorMaps):
-
     @property
     def dimension_list(self):
-        return ['x','y']
+        return ["x", "y"]
 
 
 class Lartpc2D:
-
     def __init__(self, result_dimension, max_step_number):
         self.result_dimenstion = result_dimension
         self.detector = Detector2D(result_dimension)
-        self.cursor = Cursor2D(output_size=1, input_result_size = 5, input_source_size=5, movement_size=3)
+        self.cursor = Cursor2D(
+            output_size=1, input_result_size=5, input_source_size=5, movement_size=3
+        )
         self.cursor_history = []
         self.reward_history = []
         self.max_step_number = max_step_number
         self.step_number = None
         self.done = None
-        self.action_settings = Action2DSettings(self.cursor.copy(), categories=self.detector.result_dimensions)
-        self.observation_settings = Observation2DSettings(self.cursor.copy(), categories=self.detector.result_dimensions)
-
+        self.action_settings = Action2DSettings(
+            self.cursor.copy(), categories=self.detector.result_dimensions
+        )
+        self.observation_settings = Observation2DSettings(
+            self.cursor.copy(), categories=self.detector.result_dimensions
+        )
 
     def move_cursor(self, new_center):
         self.cursor_history.append(self.cursor.current_center.copy())
@@ -88,12 +99,13 @@ class Lartpc2D:
 
     def _outside_marigin(self, new_center):
         marigin = self.cursor.region_source_input.r_low
-        return np.any(new_center <= marigin) or np.any(new_center >= self.detector.source_map.shape[0]-marigin)
-
+        return np.any(new_center <= marigin) or np.any(
+            new_center >= self.detector.source_map.shape[0] - marigin
+        )
 
     def _act(self, action: Action2Dai) -> bool:
-        assert action.put_data.shape==self.cursor.region_output.shape+(3,)
-        assert action.movement_vector.shape==(1,2)
+        assert action.put_data.shape == self.cursor.region_output.shape + (3,)
+        assert action.movement_vector.shape == (1, 2)
         self.cursor.set_range(self.detector.result_map, action.put_data)
         new_center = self.cursor.current_center + np.squeeze(action.movement_vector)
         if self._outside_marigin(new_center):
@@ -105,8 +117,8 @@ class Lartpc2D:
 
     def step(self, action: Action2Dai) -> State2Dai:
         done = True
-        can_move = self.step_number < self.max_step_number -1
-        last_move = self.step_number == self.max_step_number -1
+        can_move = self.step_number < self.max_step_number - 1
+        last_move = self.step_number == self.max_step_number - 1
         if can_move:
             action_success = self._act(action)
             # if action succedded, then it is not done
@@ -116,16 +128,22 @@ class Lartpc2D:
             else:
                 done = True
         self.done = done
-        #if np.count_nonzero(self.get_observation().source) == 0:
+        # if np.count_nonzero(self.get_observation().source) == 0:
         #    done = True
         state = self.get_state()
         self.reward_history.append(state.reward)
         return state
 
     def get_observation(self) -> Observation2Dai:
-        source_curs = self.cursor.get_range(self.detector.source_map, region_type='source_input').astype(np.float32)
-        result_curs = self.cursor.get_range(self.detector.result_map, region_type='result_input').astype(np.float32)
-        target_curs = self.cursor.get_range(self.detector.target_map, region_type='output').astype(np.int32)
+        source_curs = self.cursor.get_range(
+            self.detector.source_map, region_type="source_input"
+        ).astype(np.float32)
+        result_curs = self.cursor.get_range(
+            self.detector.result_map, region_type="result_input"
+        ).astype(np.float32)
+        target_curs = self.cursor.get_range(
+            self.detector.target_map, region_type="output"
+        ).astype(np.int32)
         obs = Observation2Dai(source_curs, result_curs, target_curs)
         return obs
 
@@ -139,23 +157,30 @@ class Lartpc2D:
             result_categorised = np.argmax(result_cursor, axis=2)
         else:
             result_categorised = result_cursor
-        center_pixel = source_cursor[game.cursor.region_source_input.r_low, game.cursor.region_source_input.r_low]
-        discovered_pixels = game.cursor.region_result_input.basic_block_size - np.count_nonzero(result_categorised)
-        reward = nonzero_source_px+discovered_pixels*.09
-        assert reward>=0
-        if discovered_pixels== 0:
+        center_pixel = source_cursor[
+            game.cursor.region_source_input.r_low, game.cursor.region_source_input.r_low
+        ]
+        discovered_pixels = (
+            game.cursor.region_result_input.basic_block_size
+            - np.count_nonzero(result_categorised)
+        )
+        reward = nonzero_source_px + discovered_pixels * 0.09
+        assert reward >= 0
+        if discovered_pixels == 0:
             reward = reward - 5
-        center_pixel_is_zero =  (np.count_nonzero(center_pixel) == 0 )
+        center_pixel_is_zero = np.count_nonzero(center_pixel) == 0
         if center_pixel_is_zero:
-            reward = reward-15
-            #if self.reward_history <= 0.0:
+            reward = reward - 15
+            # if self.reward_history <= 0.0:
             #    reward = reward - self.reward_history[-1]*3
         return reward
 
     def reward(self):
         rewards_dict = dict(
             source_cursor=self.cursor.get_range(self.detector.source_map),
-            result_cursor=self.cursor.get_range(self.detector.result_map, region_type='result_input'),
+            result_cursor=self.cursor.get_range(
+                self.detector.result_map, region_type="result_input"
+            ),
         )
         reward = Lartpc2D._reward_calc(self, **rewards_dict)
         return reward
