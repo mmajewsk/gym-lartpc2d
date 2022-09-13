90a3358d91d2dd735c5d114b09dfd658edec7088
mwmajewsk <5279578+mmajewsk@users.noreply.github.com>
2022-09-13 13:02:41 +0200

    blackify lartpc.py

--- gym_lartpc/envs/lartpc.py
+++ gym_lartpc/envs/lartpc.py
@@ -4,26 +4,27 @@ from gym.utils import seeding
 from lartpc_game.game.game_ai import Lartpc2D
 from lartpc_game.agents.observables import Action2Dai, State2Dai, Observation2Dai
 
+
 class LartpcEnv(gym.Env, Lartpc2D):
-  metadata = {'render.modes': ['human']}
+    metadata = {"render.modes": ["human"]}
 
-  def __init__(self, result_dimension=3, max_step_number=8):
-      gym.Env.__init__(self)
-      Lartpc2D.__init__(self, result_dimension, max_step_number)
+    def __init__(self, result_dimension=3, max_step_number=8):
+        gym.Env.__init__(self)
+        Lartpc2D.__init__(self, result_dimension, max_step_number)
 
-  def step(self, action: Action2Dai) -> State2Dai:
-      return Lartpc2D.step(self, action)
+    def step(self, action: Action2Dai) -> State2Dai:
+        return Lartpc2D.step(self, action)
 
-  def set_maps(self, src, trgt):
-      self.detector.set_maps(src, trgt)
+    def set_maps(self, src, trgt):
+        self.detector.set_maps(src, trgt)
 
-  def reset(self):
-      src, trgt = self.detector.source_map, self.detector.target_map
-      self.detector.set_maps(src, trg)
-      Lartpc2D.start(self)
+    def reset(self):
+        src, trgt = self.detector.source_map, self.detector.target_map
+        self.detector.set_maps(src, trg)
+        Lartpc2D.start(self)
 
-  def render(self, mode='human'):
-      raise NotImplementedError('Use vis.py insted')
+    def render(self, mode="human"):
+        raise NotImplementedError("Use vis.py insted")
 
-  def close(self):
-      pass
+    def close(self):
+        pass
